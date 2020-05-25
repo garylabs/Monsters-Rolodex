6b77fbfb2d7be7b7e024ec7c4512383c0c771120
Gurveer Singh <gurveer.singh1608@gmail.com>
2020-05-25 01:08:28 -0400

    Updates

---
+++ precache-manifest.621bed2ac14e34bd308d4ab972ac6854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d92303396d51a86129165e0e17301c1",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "8669db374622670ecc07",
+    "url": "/Monsters-Rolodex/static/css/main.a24341fc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8669db374622670ecc07",
+    "url": "/Monsters-Rolodex/static/js/main.12123e6a.chunk.js"
+  },
+  {
+    "revision": "bb1db29ffdd7a4ded20c",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.f852ebba.js"
+  }
+]);
